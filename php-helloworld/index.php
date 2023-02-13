71f25b85edb1277412093fc91e0a36bc8b72351c
Student User <student@workstation.lab.example.com>
2023-02-13 15:54:42 -0500

    changes added

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming \n";
-print "A change made from workstation \n";
-print "Additional changes made and build from GUI \n";
+print "Changes are made from workstation. \n"
 ?>
